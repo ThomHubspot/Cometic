6632f9c8697d441e8877f51bcdc0a0340fc79c64
ThomHubspot <129523852+ThomHubspot@users.noreply.github.com>
2023-10-04 11:59:01 +0200

    Update example-card.js

--- src/app/serverless/example-card.js
+++ src/app/serverless/example-card.js
@@ -19,18 +19,7 @@ exports.main = async (context = {}, sendResponse) => {
   }, {
     "type": "divider",
     "distance": "small"
-  },  {
-    "type": "heading",
-    "text": "Images"
-  },{
-    "type": "image",
-    "src": "https://v.seloger.com/s/width/1440/visuels/1/7/6/b/176b4xvn1yu2hx7emnqozkq8sf29p9g8sxx5ns5ts.jpg",
-    "alt": "Photo-bien",
-    "onClick": {
-      "type": "SERVERLESS_ACTION_HOOK",
-      "serverlessFunction": "exampleFunction"
-    }
-  },{
+  }, {
     "type": "divider",
     "distance": "small"
   },{
